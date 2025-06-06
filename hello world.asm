429aa6fdd73d9b6e0f913e1fe49d5aa9f8a28131
Russell Bulmer <russell.bulmer@gmail.com>
2025-06-06 15:31:04 +0100

    Refactor :)

--- hello world.asm
+++ hello world.asm
@@ -1,28 +1,8 @@
 	DEVICE ZXSPECTRUM48 			; needed for SNA export (must be tab indented)
 	org $8000
 	
+	INCLUDE "speccy_defs.asm"		; must be indented
 	
-; character codes (must be left-aligned)	
-C_ENTER		= $0D
-C_AT		= $16
-
-; colours
-COL_BLK		= $0
-COL_BLU		= $1
-COL_RED		= $2
-COL_MAG		= $3
-COL_GRN		= $4
-COL_CYN		= $5
-COL_YEL		= $6
-COL_WHT		= $7
-COL_C_INK	= $10
-COL_C_PAP	= $11
-COL_C_FLA	= $12
-COL_C_BRI	= $13
-
-; ROM calls
-ROM_CLS		= $0DAF					; cls and open Channel 2 
-ROM_BORDER	= $229B					; set border to value in a
 
 START:
 	ld		a, COL_BLU				; blue in a
@@ -33,10 +13,36 @@ START:
 MY_LOOP:
 	ld 		a, (bc)					; load char from MY_STRING to a
 	cp 		255						; check for 255 terminator
-	jr 		z, END_PROGRAM			; end if we're done
+	jr 		z, UDG			; end if we're done
 	rst 	$10						; ROM print the char (and move cursor)
 	inc 	bc						; next char in string
 	jr 		MY_LOOP					; iterate
+
+
+UDG: 								; just proving I can change UDGs each time I print
+	ld		a, C_UDG_1
+	rst		$10
+	
+	ld		b, 7
+	ld		HL, UDG_START
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+	inc		HL
+	ld		(HL), $77
+
+	ld		a, C_UDG_1
+	rst		$10
 
 END_PROGRAM:
 	ld		a, COL_GRN				; random border shit depending on timings...
@@ -54,6 +60,7 @@ MY_STRING:
 	defb 	"Hello World!"			
 	defb 	C_AT, 21, 0, COL_C_INK, COL_BLK	
 	defb 	"Bottom"			
+	defb 	C_AT, 5, 0
 	defb 	255				
   
   
